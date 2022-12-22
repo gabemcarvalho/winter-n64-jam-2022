9500d4c839499224564217e84aca5db8b40699dc
blhomstein <blhoms23@gmail.com>
2022-12-22 16:21:02 +0100

    scene conflict fix

--- Assets/Scripts/MiniGameScript.cs
+++ Assets/Scripts/MiniGameScript.cs
@@ -17,7 +17,9 @@ public class MiniGameScript : MonoBehaviour
     // to disable/enable the player or the gun
     public GameObject player;
     public GameObject gun;
+    GameObject playerShadow;
     [SerializeField] GameObject tree;
+    Vector3 gamePosition = new Vector3(0,0,1000);
     private PlayerController playingCharacter = new PlayerController();
 
     // ui elements such as : npc specific needs list, timer...
@@ -26,6 +28,7 @@ public class MiniGameScript : MonoBehaviour
     bool startMinigame = false;
     Vector3 playerOldPosition;
     Vector3 cameraOldPosition;
+    Vector3 treeOldPosition;
     private float rotationSpeed = 30f;
     bool aiming;
 
@@ -37,7 +40,13 @@ public class MiniGameScript : MonoBehaviour
     private void Start()
     {
         miniGameCamera = mainCamera.gameObject.GetComponent<CameraController>();
+<<<<<<< Updated upstream
         //playingCharacter = player.gameObject.GetComponent<PlayerController>();
+=======
+        playingCharacter = player.gameObject.GetComponent<PlayerController>();
+        playerShadow = GameObject.Find("Player Shadow");
+
+>>>>>>> Stashed changes
         miniSnowballPrefab = Resources.Load(snowballPrefabResource) as GameObject;
         //UIActions.EventActiveDecorationChanged?.Invoke(playingCharacter.availableDecorations[playingCharacter.activeDecorationIndex]);
 
@@ -127,9 +136,34 @@ public class MiniGameScript : MonoBehaviour
         // storing old position of camera & player
         cameraOldPosition = mainCamera.transform.position;
         playerOldPosition = player.transform.position;
+<<<<<<< Updated upstream
 
         //miniGameCamera.freeForm.enabled = false;
         miniGameCamera.lockOnTarget.followTarget = target;
+=======
+        //treeOldPosition = tree.transform.position;
+        //tree.transform.position = gamePosition;
+        mainCamera.GetComponent<CameraController>().enabled = false;
+        mainCamera.transform.LookAt(tree.transform.position);
+        player.transform.position = mainCamera.transform.position + new Vector3(0,0,1);
+        tree.layer = 13;
+        playerShadow.SetActive(false);
+        LayerCullingHide(mainCamera, 6);
+        hide(mainCamera, "Ground");
+        LayerCullingHide(mainCamera, 14);
+        hide(mainCamera, "Tree");
+        LayerCullingHide(mainCamera, 8);
+        hide(mainCamera, "NPC");
+        LayerCullingHide(mainCamera, 4);
+        hide(mainCamera, "Water");
+        player.transform.gameObject.SetActive(false);
+        //gun.GetComponent<Bucket>().followTransform = null ;
+
+        //miniGameCamera.freeForm.enabled = false;
+        //miniGameCamera.lockOnTarget.followTarget = target;
+        
+        //playingCharacter.bucketTransform.position = mainCamera.transform.position + new Vector3(0, 0, 0);
+>>>>>>> Stashed changes
 
         Vector3 targetPos = target.transform.position;
         Vector3 lineOfSight = new Vector3(targetPos.x - 10, 0, targetPos.z - 10).normalized;
@@ -138,6 +172,7 @@ public class MiniGameScript : MonoBehaviour
 
 
         // masking undesirable layers during the play 
+<<<<<<< Updated upstream
         LayerCullingHide(mainCamera, 6);
         hide(mainCamera, "Ground");
         LayerCullingHide(mainCamera, 12);
@@ -146,11 +181,14 @@ public class MiniGameScript : MonoBehaviour
         hide(mainCamera, "NPC");
         LayerCullingHide(mainCamera, 4);
         hide(mainCamera, "Water");
+=======
+>>>>>>> Stashed changes
 
 
         Debug.Log("trigger enter");
 
     }
+<<<<<<< Updated upstream
     void ExitMiniGame() {
         miniGameCamera.lockOnTarget.followTarget = null;
         player.transform.gameObject.SetActive(true);
@@ -159,6 +197,25 @@ public class MiniGameScript : MonoBehaviour
         show(mainCamera, "Ground");
         LayerCullingShow(mainCamera, 12);
         show(mainCamera, "Tree");
+=======
+    void ExitMiniGame()
+    {
+        //tree.transform.position = treeOldPosition;
+        player.transform.gameObject.SetActive(true);
+        playerShadow.SetActive(true);
+        //miniGameCamera.lockOnTarget.followTarget = null;
+        playingCharacter.OnFellInLake();
+        this.GetComponent<BoxCollider>().enabled = true;
+        mainCamera.GetComponent<CameraController>().enabled = true ;
+        tree.layer = 14;
+        //gun.GetComponent<Bucket>().followTransform = player.transform;
+        LayerCullingShow(mainCamera, 6);
+        show(mainCamera, "Ground");
+        LayerCullingShow(mainCamera, 14);
+        show(mainCamera, "Tree");
+        //LayerCullingShow(mainCamera, 12);
+        //show(mainCamera, "Tree");
+>>>>>>> Stashed changes
         LayerCullingShow(mainCamera, 8);
         show(mainCamera, "NPC");
         LayerCullingShow(mainCamera, 4);
